f629f11b04d9039fe608c2447f53f5a9bd0b9961
Helder Grilo <201149399+hgrilo-code@users.noreply.github.com>
2026-08-21 08:43:13 +0100

    Atualiza build web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1825073586" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2475323172" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
